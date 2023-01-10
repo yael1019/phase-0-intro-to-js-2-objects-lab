9435351280f201bd37e3474d4959f3c3e19262fd
Yael Harosh <yaelharosh01@gmail.com>
2023-01-10 14:06:36 -0500

    pass object lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+const employee = {
+    name: 'Yael',
+    streetAddress: 'Brooklyn, NY'
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const obj = {
+        ...employee,
+        [key]: value
+    }
+    return obj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const obj = {
+        ...employee
+    };
+    delete obj[key];
+    return obj;
+}
+
+// console.log(deleteFromEmployeeByKey(employee, 'name'));
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+
